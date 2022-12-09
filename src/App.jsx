636a71ca22debdd3bb620991a727d5ef71c7e972
Nabil <nabil.ziani@brightest.be>
2022-12-09 22:18:46 +0100

    added postcss config + implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import styles from './styles'
+import styles from "./style";
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flextCenter}´`}>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-    <div className={`${styles.boxWidth}`}>
-        Hero
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
       </div>
     </div>
-
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-    <div className={`${styles.boxWidth}`}>
-        Stats 
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
-
   </div>
-)
+);
 
-export default App
+export default App;
